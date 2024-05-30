95fd3958a95d63101b2751c8c58110bf83cfc2e0
Gautam Raj <122704823+gautamraj5488@users.noreply.github.com>
2024-05-30 07:04:36 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,9 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar:AppBar()
+    );
   }
 }
 
